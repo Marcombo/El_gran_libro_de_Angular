8039b65c3f78040d91bad4f758d01c58c90d3152
MiquelBO <mboriols@hotmail.com>
2021-02-06 17:05:34 +0100

    ej013 2

--- projects/ej013-modcrea/src/app/app.module.ts
+++ projects/ej013-modcrea/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Mod2Module } from './mod2.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Mod2Module
   ],
   providers: [],
   bootstrap: [AppComponent]

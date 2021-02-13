8d1197c4e7cf2e6a3988f267b87748fb88e6c4e0
MiquelBO <mboriols@hotmail.com>
2021-02-13 15:50:59 +0100

    ej024

---
+++ projects/ej024-dirngif/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '024 dirNgIf';
+  grupo1=false;
+  grupo2=false;
+  subgrupo11=false;
+  subgrupo12=false;
+  subgrupo21=false;
+  subgrupo22=false;
+}

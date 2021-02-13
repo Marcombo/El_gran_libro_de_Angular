41cb29715f1572325319aa169cb380faf35559fc
MiquelBO <mboriols@hotmail.com>
2021-02-13 15:53:56 +0100

    ej025

---
+++ projects/ej025-dirngfor/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = '025 dirNgFor';
+  public nombres = [
+    {nom:"uno",edad:10},
+    {nom:"dos",edad:20},
+    {nom:"tres",edad:30}
+  ];
+}

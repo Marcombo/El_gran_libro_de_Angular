11b99d924af224bc4d48069a6397d4ebcfa9f847
MiquelBO <mboriols@hotmail.com>
2021-02-20 07:16:58 +0100

    ej051

--- projects/ej050u052-servicios/src/app/libro-lista/libro-lista.component.ts
+++ projects/ej050u052-servicios/src/app/libro-lista/libro-lista.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import{ Libro } from'../libro.model';
 import { LIBROS } from '../mocks';
 import { LibroService } from '../libro.service';
+import { LoggerService } from '../logger.service';
 
 @Component({
   selector: 'app-libro-lista',
@@ -10,7 +11,37 @@ import { LibroService } from '../libro.service';
 })
 export class LibroListaComponent implements OnInit {
   libros: Libro[] = [];
+  constructor(private libroService: LibroService, private loggerService: LoggerService){  }
+  
+  ngOnInit() {
+    //this.libros = this.libroService.getLibros();
+    //this.libroService.getLibros().then(libros => this.libros = libros);
 
-  constructor(private libroService: LibroService){  }
-  ngOnInit() {    this.libros = this.libroService.getLibros();  }
+    this.libroService.getLibros()
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 1º then");
+        return libros;
+      }
+    )
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 2º then");
+        return new Promise<Libro[]>((resolve, reject) => { // (*)
+            this.loggerService.log("Inicio ejecutor (Promise del 2º then)");
+            setTimeout(() => {
+              this.loggerService.log("Fin ejecutor (Promise del 2º then)");
+              resolve(libros);
+            }, 5000);
+          });
+      }
+    )
+    .then(
+      libros => {
+        this.loggerService.log("Ejecucion del 3º then");
+        this.libros = libros;
+      }
+    );
+
+  }
 }
--- projects/ej050u052-servicios/src/app/libro.service.ts
+++ projects/ej050u052-servicios/src/app/libro.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
 import { LoggerService } from './logger.service';
+import{ Libro } from'./libro.model';
 import { LIBROS } from './mocks';
 
 @Injectable()
 export class LibroService {
   constructor(private loggerService: LoggerService) { }
-
-  getLibros() {
+  /*getLibros() {
     this.loggerService.log("Llamada realizada sobre LibroService.getLibros");
     return LIBROS;
+  }*/
+
+  getLibros(): Promise<Libro[]> {
+    return new Promise<Libro[]>( (resolve, reject) => {
+      this.loggerService.log("Inicio ejecutor (Promise de LibroService.getLibros())");
+      setTimeout(() => {
+        this.loggerService.log("Fin ejecutor (Promise de LibroService.getLibros())");
+        resolve(LIBROS);
+      }, 5000);
+    });
   }
 }

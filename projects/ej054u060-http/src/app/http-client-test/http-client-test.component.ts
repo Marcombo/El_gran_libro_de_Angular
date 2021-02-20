cf70d8dccbbad85e1a0c068ec6c9e0c7de6643d2
MiquelBO <mboriols@hotmail.com>
2021-02-20 07:53:45 +0100

    ej055

--- projects/ej054u060-http/src/app/http-client-test/http-client-test.component.ts
+++ projects/ej054u060-http/src/app/http-client-test/http-client-test.component.ts
@@ -7,10 +7,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./http-client-test.component.css']
 })
 export class HttpClientTestComponent implements OnInit {
-
+  resultadoPeticion: Object | undefined;
   constructor(private http: HttpClient) { }
+  ngOnInit() { this.get(); }
 
-  ngOnInit() {
+  get() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts')
+      .subscribe( data => { this.resultadoPeticion = data; } );
   }
-
+  post(){
+    this.http.post('https://jsonplaceholder.typicode.com/posts',
+      {
+      title: 'Previsión Viernes.',
+      body: 'Parcialmente soleado.',
+      userId: 1
+      })
+      .subscribe( data => { this.resultadoPeticion = data; } );
+  }
 }

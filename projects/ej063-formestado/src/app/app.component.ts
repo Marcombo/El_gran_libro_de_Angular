b770f1edf5c6f43a2db639834993f49213fa9167
MiquelBO <mboriols@hotmail.com>
2021-02-28 16:07:47 +0100

    ej063

--- projects/ej063-formestado/src/app/app.component.ts
+++ projects/ej063-formestado/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   title = '063 formEstado';
+  @ViewChild('miForm') miForm!: NgForm;
+
+  constructor(private ref: ChangeDetectorRef) {}
+
+  ngAfterViewInit () {
+    this.miForm.valueChanges?.subscribe(()  => {
+        setTimeout (() => { this.ref.markForCheck(); }, 100)
+    });
+  }
 }

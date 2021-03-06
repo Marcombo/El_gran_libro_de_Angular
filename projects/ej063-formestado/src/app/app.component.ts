345d19b3c1a8c6810dae679cd0e3365ae1929a87
MiquelBO <mboriols@hotmail.com>
2021-03-06 16:49:35 +0100

    ej063

--- projects/ej063-formestado/src/app/app.component.ts
+++ projects/ej063-formestado/src/app/app.component.ts
@@ -1,21 +1,36 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { interval, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = '063 formEstado';
-  @ViewChild('miForm') miForm!: NgForm;
+  codigoClassName: Observable<string>|null = null;
+  nombreClassName: Observable<string>|null = null;
+  emailClassName: Observable<string>|null = null;
+  movilClassName: Observable<string>|null = null;
+  @ViewChild('codigo') codigo!: ElementRef;
+  @ViewChild('nombre') nombre!: ElementRef;
+  @ViewChild('email') email!: ElementRef;
+  @ViewChild('movil') movil!: ElementRef;
 
-  constructor(private ref: ChangeDetectorRef) {}
+  ngAfterViewInit() {
+    this.codigoClassName = this.className(this.codigo.nativeElement);
+    this.nombreClassName = this.className(this.nombre.nativeElement);
+    this.emailClassName = this.className(this.email.nativeElement);
+    this.movilClassName = this.className(this.movil.nativeElement);
+  }
 
-  ngAfterViewInit () {
-    this.miForm.valueChanges?.subscribe(()  => {
-        setTimeout (() => { this.ref.markForCheck(); }, 100)
-    });
+  className(element: any): Observable<string> {
+    return interval(500).pipe(
+      map(() => {
+        return element.className;
+      })
+    );
   }
 }

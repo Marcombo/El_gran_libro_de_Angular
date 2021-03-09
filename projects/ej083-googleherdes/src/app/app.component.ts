4ffb4cb15320e3fc2c1ff4e4d92cd1115b417ba4
MiquelBO <mboriols@hotmail.com>
2021-03-09 17:20:00 +0100

    ej083

---
+++ projects/ej083-googleherdes/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title   = '028 dirNgStyle';
+  color = 'blue';
+  hayLetraGrande:boolean  = false;
+  hayColorFondo:boolean  = false;
+  hayLetraColor:boolean  = false;
+  hayLetraItalica:boolean  = false;
+  defineEstilos(){
+      let styles= {
+          'font-size': this.hayLetraGrande ? '300%' : 'initial',
+          'background-color': this.hayColorFondo ? '#58FA58' : '#FFFFFF',
+          'color': this.hayLetraColor ? 'red' : 'black',
+          'font-style': this.hayLetraItalica ? 'italic' : 'normal'
+      };
+      return styles;
+  }
+  ngAfterContentChecked(){
+  console.log("--------------------------- ");
+  console.log("hayLetraGrande ",this.hayLetraGrande);
+  console.log("hayColorFondo ",this.hayColorFondo);
+  console.log("hayLetraColor ",this.hayLetraColor);
+  console.log("hayLetraItalica ",this.hayLetraItalica);
+  console.log("--------------------------- ");
+}
+
+}
